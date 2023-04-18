2ed1d5ff8ccec3698db45442120eea246cfd5ef7
Yeo Se Young <dutpdud132@sookmyung.ac.kr>
2023-04-18 19:29:08 +0900

    8번의 5번까지 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Todo from './Todo';
+import {Paper, List}from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component { 
@@ -7,18 +8,24 @@ class App extends React.Component {
     super(props);
     this.state ={
       items :[ 
-        {id:0, title:"Todo 1 ", done:false}, 
+        {id:0, title:"Todo 1 ", done:true}, 
         {id:1, title:"Todo 2 ", done:false}, 
       ],
     }; 
   } 
   render(){
-    var todoItems =this.state.items.map((item,idx)=>(
-      <Todo item={item} key={item.id}/>
-    ));
-     
-    return <div className="App">{todoItems}</div>;
-  } 
+    var todoItems =this.state.items.length >0 &&(
+      <Paper style={{margin:16}}>
+        <List>
+        {this.state.items.map((item,idx)=>(
+        <Todo item={item} key={item.id}/>
+        ))} 
+        </List>
+      </Paper>
+    );
+      
+    return <div className="App">{todoItems}</div>; 
+  }
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ListItem, ListItemText, InputBase, Checkbox}from "@material-ui/core";
 
 class Todo extends React.Component {
     constructor(props){
@@ -6,17 +7,22 @@ class Todo extends React.Component {
         this.state ={ item: props.item };
     }
 
-    render(){ 
+    render(){
+        const item =this.state.item;
         return (
-            <div className="Todo">
-                <input
-                    type="checkbox"
-                    id={this.state.item.id}
-                    name={this.state.item.id} 
-                    checked={this.state.item.done}
-                />
-                <label for={this.state.item.id}>{this.state.item.title}</label>
-                </div>
+            <ListItem>
+                <Checkbox checked={item.done}/>
+                <ListItemText>
+                    <InputBase
+                    inputProps={{"aria-label":"naked"}} type="text"
+                    id={item.id} 
+                    name={item.id} 
+                    value={item.title} 
+                    multiline={true} 
+                    fullWidth={true}
+                    />
+                </ListItemText>
+            </ListItem>
         ); 
     }
 }
