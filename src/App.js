4796efaf538245b8afc161ff5945d5955effc17d
Yeo Se Young <dutpdud132@sookmyung.ac.kr>
2023-04-18 19:18:42 +0900

    8번의 3번까지 실행

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state ={
-      item :{id:0, title:"Hello World 1 ", done:true}, 
+      items :[ 
+        {id:0, title:"Todo 1 ", done:false}, 
+        {id:1, title:"Todo 2 ", done:false}, 
+      ],
     }; 
-  } render(){ 
-      return (
-        <div className="App">
-          <Todo item={this.state.item}/> 
-      </div>
-    ); 
+  } 
+  render(){
+    var todoItems =this.state.items.map((item,idx)=>(
+      <Todo item={item} key={item.id}/>
+    ));
+     
+    return <div className="App">{todoItems}</div>;
   } 
 }
 
--- src/Todo.js
+++ src/Todo.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 class Todo extends React.Component {
     constructor(props){
-        super(props); this.state ={ item: props.item };
-    } 
+        super(props); 
+        this.state ={ item: props.item };
+    }
+
     render(){ 
         return (
             <div className="Todo">
